b2314b0cf9715f6d6b22e5c970cde1b3794b46d5
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-09 23:14:26 +0530

    Update ai_LazyDeveloper.py

--- ai_LazyDeveloper.py
+++ ai_LazyDeveloper.py
@@ -1,6 +1,6 @@
 #
 #         i am NOT INTRESTED IN BEING YOUR SECOND FATHER... SO DON'T REMOVE MY CREDIT...
-#                                       @LazyDeveloperr
+#                                       @KANHADeveloper
 # 
 # 
 # 
@@ -15,7 +15,7 @@
 #                                                ...PRESENTING...
 #                                  A R T I F i C I A L  -  I N T E L i G E N C E
 #                                              .in  Auto-Filter-Bot.
-#                                                 @LazyDeveloperr
+#                                                 @KANHADeveloper
 #
 #                                       
 #
@@ -32,9 +32,9 @@
 #         ...while you are removing my credit and calling yourself a developerr... 
 #         _____ just imagine, At that time i am fucking your mom and sis at same time, harder & too harder...
 #
-# Credit @LazyDeveloper.
+# Credit @KANHADeveloper.
 # Please Don't remove credit.
-# Born to make history @LazyDeveloper !
-# Thank you LazyDeveloper for helping us in this Journey
-# 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
-# for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
+# Born to make history @KANHADeveloper !
+# Thank you KANHADeveloper for helping us in this Journey
+# 🥰  Thank you for giving me credit @KANHADeveloper  🥰
+# for any error please contact me -> telegram@KANHADeveloper or insta @KANHADeveloper 
