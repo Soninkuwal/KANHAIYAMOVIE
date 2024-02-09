e72327443ded6ad29f4ae7a78b8ce440881338eb
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-10 01:54:49 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -1,5 +1,5 @@
-# with Love @LazyDeveloperr 💘
-# Subscribe YT @LazyDeveloperr - to learn more about this for free...
+# with Love @KANHADeveloper 💘
+# Subscribe YT @KANHADeveloper - to learn more about this for free...
 
 import io
 from pyrogram import filters, Client, enums
