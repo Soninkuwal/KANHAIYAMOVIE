b84b4327f7ff893fee720f8a7768f248e363937f
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-10 12:02:50 +0530

    Update rlazy_thumbnail.py

--- plugins/rlazy_thumbnail.py
+++ plugins/rlazy_thumbnail.py
@@ -1,11 +1,10 @@
-    # Credit @LazyDeveloper.
-    # Please Don't remove credit.
-    # Born to make history @LazyDeveloper !
-
-    # Thank you LazyDeveloper for helping us in this Journey
-    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
-
-    # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
+# Credit @KANHADeveloper.
+# Please Don't remove credit.
+# Born to make history @KANHADeveloper !
+# Thank you KANHADeveloper for helping us in this Journey
+# 🥰  Thank you for giving me credit @KANHADeveloper  🥰
+# for any error please contact me -> telegram@KANHADeveloper or insta @KANHADeveloper 
+# rip paid developers 🤣 - >> No need to buy paid source code while @KANHADeveloper is here 😍😍 
 
 from pyrogram import Client, filters
 import logging
