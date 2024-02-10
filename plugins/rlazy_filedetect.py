02888825c61678332fc4055924fb16d0b6b48f65
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-10 12:01:33 +0530

    Update rlazy_filedetect.py

--- plugins/rlazy_filedetect.py
+++ plugins/rlazy_filedetect.py
@@ -1,11 +1,12 @@
-    # Credit @LazyDeveloper.
-    # Please Don't remove credit.
-    # Born to make history @LazyDeveloper !
+# Credit @KANHADeveloper.
 
-    # Thank you LazyDeveloper for helping us in this Journey
-    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+# Please Don't remove credit.
 
-    # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
+# Born to make history @KANHADeveloper !
+# Thank you KANHADeveloper for helping us in this Journey
+# 🥰  Thank you for giving me credit @KANHADeveloper  🥰
+# for any error please contact me -> telegram@KANHADeveloper or insta @KANHADeveloper 
+# rip paid developers 🤣 - >> No need to buy paid source code while @KANHADeveloper is here 😍😍
 
 from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
