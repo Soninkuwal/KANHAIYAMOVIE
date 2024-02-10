c346b96e7eb5f63c750a9f9c9505abe6a4c070cc
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-10 12:24:05 +0530

    Update add.py

--- database/add.py
+++ database/add.py
@@ -1,5 +1,5 @@
-# with Love @LazyDeveloperr 💘
-# Subscribe YT @LazyDeveloperr - to learn more about this for free...
+# with Love @KANHADeveloper 💘
+# Subscribe YT @KANHADeveloper - to learn more about this for free...
 
 from database.users_chats_db import db
 from pyrogram import Client
