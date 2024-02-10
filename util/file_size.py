bd5b7a532e34fd29fa104e4fb2d2dc669e1fac2e
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-10 12:42:25 +0530

    Update file_size.py

--- util/file_size.py
+++ util/file_size.py
@@ -1,10 +1,16 @@
-# Credit @LazyDeveloper.
+# Credit @KANHADeveloper.
+
 # Please Don't remove credit.
-# Born to make history @LazyDeveloper !
-# Thank you LazyDeveloper for helping us in this Journey
-# 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
-# for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
-# rip paid developers 🤣 - >> No need to buy paid source code while @LazyDeveloperr is here 😍😍
+
+# Born to make history @KANHADeveloper !
+
+# Thank you KANHADeveloper for helping us in this Journey
+
+# 🥰  Thank you for giving me credit @KANHADeveloper  🥰
+
+# for any error please contact me -> telegram@KANHADeveloper or insta @KANHADeveloper 
+
+# rip paid developers 🤣 - >> No need to buy paid source code while @KANHADeveloper is here 😍😍
 def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
     """ Returns a human readable string representation of bytes """
     return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
